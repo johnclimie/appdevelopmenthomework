6d133eb40fc97dcd99f7022b65cda6097f353761
John Climie <johnclimie00@gmail.com>
2023-08-29 22:53:39 -0400

    Returns discount amount

--- Week3/assignmentTwo.py
+++ Week3/assignmentTwo.py
@@ -9,14 +9,18 @@ input = float(input('Enter how many packages you would like: ')) # User is promp
 if input < 10: # Returns no discount and total when under 10 purchased
     print("You have no discount, making your total", price*input)
 elif input >= 10 and input <= 19: # Returns 10% discount and total when under 20 purchased
+    discount = price*.10*input
     finalPrice = price*.90*input
-    print(f"You have a 10 percent discount, making your total ${finalPrice:,.2f}")
+    print(f"You have a 10 percent discount of ${discount:,.2f}, making your total ${finalPrice:,.2f}")
 elif input >= 20 and input <= 49: # Returns 20% discount and total when under 50 purchased
+    discount = price*.20*input
     finalPrice = price*.80*input
-    print(f"You have a 20 percent discount, making your total ${finalPrice:,.2f}")
+    print(f"You have a 20 percent discount of ${discount:,.2f}, making your total ${finalPrice:,.2f}")
 elif input >= 50 and input <= 99: # Returns 30% discount and total when under 100 purchased
+    discount = price*.30*input
     finalPrice = price*.70*input
-    print(f"You have a 30 percent discount, making your total ${finalPrice:,.2f}")
+    print(f"You have a 30 percent discount of ${discount:,.2f}, making your total ${finalPrice:,.2f}")
 else: # Returns 40% discount and total when over 100 purchased
+    discount = price*.40*input
     finalPrice = price*.60*input
-    print(f"You have a 40 percent discount, making your total ${finalPrice:,.2f}")
+    print(f"You have a 40 percent discount of ${discount:,.2f}35, making your total ${finalPrice:,.2f}")
