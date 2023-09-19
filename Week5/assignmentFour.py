2a3b46ae06d2c9db1b7cd81e83535951c20362bd
John Climie <johnclimie00@gmail.com>
2023-09-19 13:09:19 -0400

    Added comments

---
+++ Week5/assignmentFour.py
@@ -0,0 +1,12 @@
+def main():
+    pass
+
+# Create function that returns gallons of paint required
+
+# Create a funcion that returns hours of labor
+
+# Create a function that returns cost of paint
+
+# Create a function that returns labor charge
+
+# Create a funciton that returns the total cost
